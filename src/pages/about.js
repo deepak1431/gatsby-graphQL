7c491719ac82892e1cfa6a4355d405f96c4e2b19
mqfullstack-2 <deepak.multiqos@gmail.com>
2022-06-08 18:45:25 +0530

    site

---
+++ src/pages/about.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Layout from './components/Layout'
+
+export default function About()  {
+  return (
+      <Layout>
+    <div>
+        <h1>About Page</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec risus in odio rhoncus facilisis.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec risus in odio rhoncus facilisis.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec risus in odio rhoncus facilisis.</p>
+    </div>
+    </Layout>
+  )
+}
+
